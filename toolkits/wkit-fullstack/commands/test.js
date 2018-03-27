8644fb1e582896f0f6a9ee1f40f9afb94cfcb109
Ran Yitzhaki <ranyitz@gmail.com>
2018-03-27 17:27:33 +0300

    intial test setup with jest

--- toolkits/wkit-fullstack/commands/test.js
+++ toolkits/wkit-fullstack/commands/test.js
@@ -1,5 +1,21 @@
+const jest = require('jest');
+
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-console.log('testing!!!');
+process.env.NODE_ENV = 'test';
+
+const argv = process.argv.slice(2);
+
+// add the watch option unless on CI, coverage mode, or watchAll mode
+if (
+  !process.env.CI &&
+  argv.indexOf('--coverage') === -1 &&
+  argv.indexOf('--watchAll') === -1 &&
+  argv.indexOf('--watch') === -1
+) {
+  argv.push('--watch');
+}
+
+jest.run(argv);
