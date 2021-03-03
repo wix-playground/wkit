e689388e6e513ec13e5706336369a8c597f93782
Dean Shub <deanshub@gmail.com>
2021-03-03 06:49:13 +0000

    Nissix "scope-packages" plugin

--- generated-example/src/test-setup.js
+++ generated-example/src/test-setup.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { wixAxiosConfig } from 'wix-axios-config';
+import { wixAxiosConfig } from '@wix/wix-axios-config';
 import { baseURL } from '../test/test-common';
 
 wixAxiosConfig(axios, { baseURL });
