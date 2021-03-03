2bca67cddc8da4e14d13cca21cbbfbb3cdf3d50f
Dean Shub <deanshub@gmail.com>
2021-03-03 11:28:09 +0200

    Nissix "scope-packages" plugin (#5)

--- generated-example/test/it/server.spec.js
+++ generated-example/test/it/server.spec.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 import { beforeAndAfter, app } from '../environment';
-import { wixAxiosInstanceConfig } from 'wix-axios-config';
+import { wixAxiosInstanceConfig } from '@wix/wix-axios-config';
 
 const axiosInstance = wixAxiosInstanceConfig(axios, { adapter });
 
